6a7be892d35aca2ad8683bf55baa4488bd932471
AdenWhitworth <aden.whitworth@gmail.com>
2024-08-11 14:54:54 -0700

    changed color set to be faded into

--- src/main.cpp
+++ src/main.cpp
@@ -162,6 +162,62 @@ void correctSoilCapacitive(){
 
 }
 
+/* LED */
+
+void setColor(int redValue, int greenValue, int blueValue){
+
+  static bool isFirstCall = true;
+
+  if (isFirstCall){
+    ledcAttachPin(RedLedPin, PWM1_Ch);
+    ledcSetup(PWM1_Ch, PWM1_Freq, PWM1_Res);
+    
+    ledcAttachPin(GreenLedPin, PWM2_Ch);
+    ledcSetup(PWM2_Ch, PWM2_Freq, PWM2_Res);
+    
+    ledcAttachPin(BlueLedPin, PWM3_Ch);
+    ledcSetup(PWM3_Ch, PWM3_Freq, PWM3_Res);
+    isFirstCall = false;
+  }
+  
+  if (redValue != 256){
+    redValue = 255 - redValue;
+  }
+  if (greenValue != 256){
+    greenValue = 255 - greenValue;
+  }
+  if (blueValue != 256){
+    blueValue = 255 - blueValue;
+  }
+  
+  ledcWrite(0,redValue); 
+  ledcWrite(1,greenValue);
+  ledcWrite(2,blueValue);
+}
+
+void fadeToColor(int red, int green, int blue, int fadeDuration) {
+    for (int i = 0; i <= 255; i++) {
+        setColor(red * i / 255, green * i / 255, blue * i / 255);
+        delay(fadeDuration);
+    }
+}
+
+void fadeInAndOutColor(int red, int green, int blue, int fadeDuration) {
+    while (keepBlinking) {
+        for (int i = 0; i <= 255; i++) {
+            if (!keepBlinking) return;
+            setColor(red * i / 255, green * i / 255, blue * i / 255);
+            delay(fadeDuration);
+        }
+        // Fade out to black
+        for (int i = 255; i >= 0; i--) {
+            if (!keepBlinking) return; 
+            setColor(red * i / 255, green * i / 255, blue * i / 255);
+            delay(fadeDuration);
+        }
+    }
+}
+
 void getAndCheckShadowState() {
   Serial.println("Retrieving initial shadow state...");
   
@@ -290,7 +346,7 @@ void connectToMQTT() {
   if (!client.connected()) {
     Serial.println("ESP32 - AWS IoT Timeout!");
     keepBlinking = false;
-    setColor(255, 0, 0); 
+    fadeToColor(255, 0, 0, 100); 
     return;
   }
 
@@ -301,64 +357,8 @@ void connectToMQTT() {
 
   Serial.println("ESP32  - AWS IoT Connected!");
   keepBlinking = false;
-  setColor(0, 255, 0);
+  fadeToColor(0,255,0,100);
   checkMQTTShadow = true; 
-}
-
-/* LED */
-
-void setColor(int redValue, int greenValue, int blueValue){
-
-  static bool isFirstCall = true;
-
-  if (isFirstCall){
-    ledcAttachPin(RedLedPin, PWM1_Ch);
-    ledcSetup(PWM1_Ch, PWM1_Freq, PWM1_Res);
-    
-    ledcAttachPin(GreenLedPin, PWM2_Ch);
-    ledcSetup(PWM2_Ch, PWM2_Freq, PWM2_Res);
-    
-    ledcAttachPin(BlueLedPin, PWM3_Ch);
-    ledcSetup(PWM3_Ch, PWM3_Freq, PWM3_Res);
-    isFirstCall = false;
-  }
-  
-  if (redValue != 256){
-    redValue = 255 - redValue;
-  }
-  if (greenValue != 256){
-    greenValue = 255 - greenValue;
-  }
-  if (blueValue != 256){
-    blueValue = 255 - blueValue;
-  }
-  
-  ledcWrite(0,redValue); 
-  ledcWrite(1,greenValue);
-  ledcWrite(2,blueValue);
-}
-
-void fadeToColor(int red, int green, int blue, int fadeDuration) {
-    for (int i = 0; i <= 255; i++) {
-        setColor(red * i / 255, green * i / 255, blue * i / 255);
-        delay(fadeDuration);
-    }
-}
-
-void fadeInAndOutColor(int red, int green, int blue, int fadeDuration) {
-    while (keepBlinking) {
-        for (int i = 0; i <= 255; i++) {
-            if (!keepBlinking) return;
-            setColor(red * i / 255, green * i / 255, blue * i / 255);
-            delay(fadeDuration);
-        }
-        // Fade out to black
-        for (int i = 255; i >= 0; i--) {
-            if (!keepBlinking) return; 
-            setColor(red * i / 255, green * i / 255, blue * i / 255);
-            delay(fadeDuration);
-        }
-    }
 }
 
 /* Data Storage */
@@ -441,7 +441,7 @@ void connectToWiFi() {
         } else {
             Serial.println("\nFailed to connect to WiFi");
             keepBlinking = false;
-            setColor(255, 0, 0); 
+            fadeToColor(255,0,0,100);
         }
     }
 }
@@ -503,9 +503,9 @@ void IRAM_ATTR statusBtnTriggered() {
     keepBlinking = false;
 
     if (client.connected()){
-      setColor(0, 255, 0); 
+      fadeToColor(0,255,0,100);
     } else {
-      setColor(255, 0, 0); 
+      fadeToColor(255, 0, 0, 100); 
     }
 
     turnOffBle();
@@ -526,7 +526,7 @@ void setup() {
 
   pinMode(statusBTNPin, INPUT_PULLUP);
 
-  setColor(255,0,0);
+  fadeToColor(255, 0, 0, 100); 
 
   loadAndDecrypt();
   connectToWiFi();
