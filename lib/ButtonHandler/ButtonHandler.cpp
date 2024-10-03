d1d9e3165f4aa8b451801f134bbde077728cc873
AdenWhitworth <aden.whitworth@gmail.com>
2024-10-03 13:35:25 -0400

    added a constants file for button handler, added debugging messages, and consolidated code

--- lib/ButtonHandler/ButtonHandler.cpp
+++ lib/ButtonHandler/ButtonHandler.cpp
@@ -5,6 +5,7 @@
 #include <Arduino.h>
 #include "../../include/PinConfig.h"
 #include "LedControlConstants.h"
+#include "ButtonHandlerConstants.h"
 
 volatile bool statusButtonPressed = false;
 
@@ -12,32 +13,28 @@ void IRAM_ATTR statusBtnTriggered() {
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
 
-  if (interrupt_time - last_interrupt_time > 200) {
+  if (interrupt_time - last_interrupt_time > ButtonSettings::DEBOUNCE_DELAY_MILLISECONDS) {
     statusButtonPressed = true;
   }
   last_interrupt_time = interrupt_time;
 }
 
 void handleStatusButtonActions(){
-  static bool bleInit = false;
-  if (!statusButtonPressed){
-    return;
-  }
+  if (!statusButtonPressed) return;
 
   if (keepBlinking) {
+    Serial.println("Stopping blinking and turning off BLE...");
     endBlinking(currentColor);
-
     turnOffBle();
   } else {
-
     if(!keepBlinking){
+      Serial.println("Starting blinking and initializing BLE...");
       if (checkMqttStatus()){
         beginBlinking(assessmentColor());
       } else {
         beginBlinking(ColorSettings::ORANGE);
       }
     }
-    
     beginBLE();
   }
 
---
+++ lib/ButtonHandler/ButtonHandlerConstants.h
@@ -0,0 +1,8 @@
+#ifndef BUTTON_HANDLER_CONSTANTS_H
+#define BUTTON_HANDLER_CONSTANTS_H
+
+namespace ButtonSettings {
+    const int DEBOUNCE_DELAY_MILLISECONDS = 200;
+}
+
+#endif
